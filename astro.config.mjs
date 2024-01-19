ab796ca18858af27ce3de12fe9f689d637ce8f87
David Huertas <davidhuertasortiz@gmail.com>
2024-01-19 20:43:20 +0100

    feat: Update Astro configuration and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [tailwind({ applyBaseStyles: false })],
+  output: 'server',
   adapter: vercel()
-});
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        TTCommons: [
+          'TTCommonsBlack',
+          'TTCommonsBold',
+          'TTCommonsDemiBold',
+          'TTCommonsMedium',
+          'TTCommonsRegular',
+          'TTCommonsExtraBold'
+        ]
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
