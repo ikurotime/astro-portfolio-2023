ac4ed60ecdabe8170798924014ba2b102d33dcc5
David Huertas <davidhuertasortiz@gmail.com>
2024-03-29 03:36:17 +0100

    new beggining

---
+++ src/blogpost.ts
@@ -0,0 +1,10 @@
+const blogpost = [
+  {
+    title: "You shouldn't use Create React App",
+    description:
+      'This is a post I made about why you should not use Create React App a couple of years ago. I still stand by this post today.',
+    href: '/blog/you-shouldnt-use-create-react-app',
+    tags: ['React', 'Create-React-App']
+  }
+]
+export default blogpost
---
+++ src/projects.ts
@@ -0,0 +1,21 @@
+const projects = [
+  {
+    title: 'LinkShortnr',
+    description: 'A classic link shortening service, made with Rust and HTMX',
+    tags: ['Rust', 'HTMX'],
+    href: 'https://github.com/orgs/linkshrtnr/repositories'
+  },
+  {
+    title: 'KuroJam',
+    description: 'A Hacktathon event website that I host anually',
+    tags: ['Astro'],
+    href: 'https://www.kurojam.com/'
+  },
+  {
+    title: 'Go API - Hexagonal Architecture',
+    description: 'Simple Go API Example using Hexagonal Architecture',
+    tags: ['Go', 'Hexagonal Architecture'],
+    href: 'https://github.com/ikurotime/go_api_clean_architecture'
+  }
+]
+export default projects
