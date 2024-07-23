857f5e25df4dd50996c33a7ea8d4e1b3ed139d6c
ikurotime <davidhuertasortiz@gmail.com>
2024-07-23 21:17:50 +0200

    refactor: Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({ 
-  site:
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4321"
-    : "https://davidhuertas.dev",
-  integrations: [tailwind(), react()]
+export default defineConfig({
+  site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://davidhuertas.dev",
+  integrations: [tailwind(), react(), sitemap()]
 });
