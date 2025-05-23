d3c0b1a32f81af790f4af580e3271efcb176f62f
David <davidhuertasortiz@gmail.com>
2025-05-23 03:01:04 +0200

    feat: Add markdown configuration with Shiki themes and improve site configuration structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://davidhuertas.dev",
-  integrations: [tailwind(), react(), sitemap()]
-});
+  site:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4321'
+      : 'https://davidhuertas.dev',
+  integrations: [tailwind(), react(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        dark: 'github-dark',
+        light: 'github-dark'
+      }
+    }
+  }
+})
